356ed151962a2c7142d62eb2243b15f2c476c018
Stefan Klinkusch <stefan.klinkusch@googlemail.com>
2019-06-06 19:13:44 +0200

    wrote sagas file

---
+++ src/sagas/index.js
@@ -0,0 +1,30 @@
+// import saga
+import { takeLatest, put, call } from "redux-saga";
+import axios from "axios";
+import {
+  API_CALL_REQUEST,
+  API_CALL_SUCCESS,
+  API_CALL_FAILURE
+} from "../actions/actionTypes";
+
+export function* watcherSaga() {
+  yield takeLatest(API_CALL_REQUEST, workerSaga);
+}
+
+function* workerSaga(action) {
+  try {
+    const number = action.number;
+    const response = yield call(() => fetchNumber(number));
+    const message = response;
+    yield put({ type: API_CALL_SUCCESS, message });
+  } catch (error) {
+    yield put({ type: API_CALL_FAILURE, error });
+  }
+}
+
+const fetchNumber = number => {
+  return axios({
+    method: "get",
+    url: `https://numbers-api-proxy-dci-fbw121.now.sh/?number=${number}`
+  });
+};
