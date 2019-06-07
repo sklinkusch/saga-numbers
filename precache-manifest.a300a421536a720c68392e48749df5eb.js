922f071a856912dc113a2f9aacca0fc755cb74d8
Stefan Klinkusch <stefan.klinkusch@googlemail.com>
2019-06-07 16:03:52 +0200

    Updates

---
+++ precache-manifest.a300a421536a720c68392e48749df5eb.js
@@ -0,0 +1,126 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8db9bbbd6424398acfb209244b87079d",
+    "url": "/saga-numbers/index.html"
+  },
+  {
+    "revision": "4755e6ee16db5a59e0c9",
+    "url": "/saga-numbers/static/css/2.81d30a77.chunk.css"
+  },
+  {
+    "revision": "5464fd7ce0e6edfd93db",
+    "url": "/saga-numbers/static/css/main.310950ef.chunk.css"
+  },
+  {
+    "revision": "4755e6ee16db5a59e0c9",
+    "url": "/saga-numbers/static/js/2.62b54b3c.chunk.js"
+  },
+  {
+    "revision": "5464fd7ce0e6edfd93db",
+    "url": "/saga-numbers/static/js/main.2b67ec88.chunk.js"
+  },
+  {
+    "revision": "bd8dcf8e3ee330e31d23",
+    "url": "/saga-numbers/static/js/runtime~main.22b5068e.js"
+  },
+  {
+    "revision": "03783c5172ee1ad128c576bf88fac168",
+    "url": "/saga-numbers/static/media/fa-brands-400.03783c51.eot"
+  },
+  {
+    "revision": "073c2f3ce60eaf69cc2767ef3d989078",
+    "url": "/saga-numbers/static/media/fa-brands-400.073c2f3c.svg"
+  },
+  {
+    "revision": "7559b3774a0625e8ca6c0160f8f6cfd8",
+    "url": "/saga-numbers/static/media/fa-brands-400.7559b377.woff2"
+  },
+  {
+    "revision": "ed2b8bf117160466ba6220a8f1da54a4",
+    "url": "/saga-numbers/static/media/fa-brands-400.ed2b8bf1.ttf"
+  },
+  {
+    "revision": "fe9d62e0d16a333a20e63c3e7595f82e",
+    "url": "/saga-numbers/static/media/fa-brands-400.fe9d62e0.woff"
+  },
+  {
+    "revision": "59215032a4397507b80e5625dc323de3",
+    "url": "/saga-numbers/static/media/fa-regular-400.59215032.ttf"
+  },
+  {
+    "revision": "8fdea4e89ac405d9f8db327adb331d8d",
+    "url": "/saga-numbers/static/media/fa-regular-400.8fdea4e8.svg"
+  },
+  {
+    "revision": "e07d9e40b26048d9abe2ef966cd6e263",
+    "url": "/saga-numbers/static/media/fa-regular-400.e07d9e40.woff2"
+  },
+  {
+    "revision": "e5770f9863963fb576942e25214a226d",
+    "url": "/saga-numbers/static/media/fa-regular-400.e5770f98.woff"
+  },
+  {
+    "revision": "fc9c63c8224fb341fc933641cbdd12ef",
+    "url": "/saga-numbers/static/media/fa-regular-400.fc9c63c8.eot"
+  },
+  {
+    "revision": "4bced7c4c0d61d4f988629bb8ae80b8b",
+    "url": "/saga-numbers/static/media/fa-solid-900.4bced7c4.woff"
+  },
+  {
+    "revision": "acf50f59802f20d8b45220eaae532a1c",
+    "url": "/saga-numbers/static/media/fa-solid-900.acf50f59.ttf"
+  },
+  {
+    "revision": "b557f56e367e59344ca95f9d1fb44352",
+    "url": "/saga-numbers/static/media/fa-solid-900.b557f56e.svg"
+  },
+  {
+    "revision": "b5cf8ae26748570d8fb95a47f46b69e1",
+    "url": "/saga-numbers/static/media/fa-solid-900.b5cf8ae2.woff2"
+  },
+  {
+    "revision": "ef3df98419d143d9617fe163bf4edc0b",
+    "url": "/saga-numbers/static/media/fa-solid-900.ef3df984.eot"
+  },
+  {
+    "revision": "15d8dd2bf3b066290ec0f35d40024c30",
+    "url": "/saga-numbers/static/media/great-vibes-v5-latin-regular.15d8dd2b.woff"
+  },
+  {
+    "revision": "4102098afc00c25359673089d8e41526",
+    "url": "/saga-numbers/static/media/great-vibes-v5-latin-regular.4102098a.ttf"
+  },
+  {
+    "revision": "44d80edbb024d08c79042218e7277f68",
+    "url": "/saga-numbers/static/media/great-vibes-v5-latin-regular.44d80edb.svg"
+  },
+  {
+    "revision": "7c23f2832c2ba4ee6a663aa56f76a8d3",
+    "url": "/saga-numbers/static/media/great-vibes-v5-latin-regular.7c23f283.eot"
+  },
+  {
+    "revision": "cd8416afbf4155a10f247c5528363c7b",
+    "url": "/saga-numbers/static/media/great-vibes-v5-latin-regular.cd8416af.woff2"
+  },
+  {
+    "revision": "02bba601716f9a0ff1e241a64cfe9b29",
+    "url": "/saga-numbers/static/media/lobster-v21-latin-regular.02bba601.woff"
+  },
+  {
+    "revision": "1eb9486645f3923a2f406b6f5373c8dc",
+    "url": "/saga-numbers/static/media/lobster-v21-latin-regular.1eb94866.ttf"
+  },
+  {
+    "revision": "4d43e5c1f491b876e1e964b37b2d18d6",
+    "url": "/saga-numbers/static/media/lobster-v21-latin-regular.4d43e5c1.svg"
+  },
+  {
+    "revision": "53b9d904b83749793527126759651519",
+    "url": "/saga-numbers/static/media/lobster-v21-latin-regular.53b9d904.eot"
+  },
+  {
+    "revision": "fe012fb7b22e5776c4a2e91889068974",
+    "url": "/saga-numbers/static/media/lobster-v21-latin-regular.fe012fb7.woff2"
+  }
+]);
