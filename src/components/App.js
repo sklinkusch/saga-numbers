5e6a4bb65add7173fb967ed0d3e4b5d336f76575
Stefan Klinkusch <stefan.klinkusch@googlemail.com>
2019-06-07 15:27:14 +0200

    self-hosted google fonts

--- src/components/App.js
+++ src/components/App.js
@@ -13,7 +13,20 @@ class App extends React.Component {
     return (
       <div className="App">
         <header className="App-header">
-          <div className="header pi">&pi;</div>
+          <div className="header pi">
+            <section className="number" id="no-a">
+              13
+            </section>
+            <section className="number" id="no-b">
+              147
+            </section>
+            <section className="number" id="no-c">
+              42
+            </section>
+            <section className="number" id="no-d">
+              27
+            </section>
+          </div>
           <div className="subheader">Numbers</div>
           <div className="flex-container flex-row">
             {message && (
